b127735e2ad3d87ef6fdb9e958a055dc1710f4c7
Hamada Aljarrah <hamadasepost@gmail.com>
2024-01-07 11:09:34 +0100

    github action test

--- src/api/auth-config.ts
+++ src/api/auth-config.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { BASE_URL } from "./apiService";
 
+
 export const oidcConfig = {
     onSignIn: async (user: any) => {
         const newUser = {
@@ -10,17 +11,15 @@ export const oidcConfig = {
         };
         console.log("from the sign");
         console.log(user.access_token);
-        
-        
+
         axios.post(BASE_URL.USER_SERVICE + "/user/create", newUser, {
             headers: { Authorization: `Bearer ${user.access_token}` },
         });
     },
-    // authority: process.env.REACT_APP_KEYCLOAK_URL || "http://localhost:8181/realms/journal-realm",
-    authority: "http://localhost:8181/realms/journal-realm",
-    clientId: "journal-client",
-    redirectUri: "http://localhost:3000",
+    authority:"http://localhost:8181/realms/journal-realm",
+    clientId:"journal-client",
+    redirectUri:"http://localhost:3000",
+    postLogoutRedirectUri: "http://localhost:3000",
     responseType: "code",
     scope: "openid profile email offline_access roles",
-    postLogoutRedirectUri: "http://localhost:3000",
 };
